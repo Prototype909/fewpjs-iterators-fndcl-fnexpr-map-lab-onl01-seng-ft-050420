2a9741538c40e0484c0f430dfd246481e09a4410
Steve Anderson <prototype909@msn.com>
2020-08-17 19:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials.map( phrase => {
-    return phrase.split('').map( word => {
+    return phrase.split(' ').map( word => {
       return word[0].toUpperCase() + word.slice(1);
-    }).join('');
+    }).join(' ');
   });
 };
